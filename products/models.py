3f49ec165165d2e502b50a1f6ffdb15a13abab39
CR Sundar <crsundar@gmail.com>
2019-06-18 10:43:22 +0530

    8th tutorial

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+	title = models.CharField(max_length=120)
+	description = models.TextField()
+	price = models.DecimalField(max_digits=7, decimal_places=2)
+	slug = models.SlugField()
+	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+	updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+	active = models.BooleanField(default=True)
+
+	def __str__(self):
+		return self.title
